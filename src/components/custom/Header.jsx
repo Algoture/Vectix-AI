1e3de458a38045fb4ef4cfeb4b3edcf27b7fff26
Umesh Nagare <umeshnagare27@gmail.com>
2025-03-23 23:21:16 +0530

    add : component

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,20 +1,9 @@
-import Link from "next/link";
-import { Logo } from "./Icons";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
-import {
-  FileText,
-  GraduationCap,
-  LayoutDashboard,
-  PenBox,
-  StarsIcon,
-} from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Logo } from "./Icons";
+import Link from "next/link";
+import { LayoutDashboard } from "lucide-react";
+import DropDown from "./DropDown";
 
 export default function Header() {
   return (
@@ -31,32 +20,7 @@ export default function Header() {
               <span className="hidden md:block">Insights</span>
             </Button>
           </Link>
-          <DropdownMenu>
-            <DropdownMenuTrigger className="cursor-pointer cen gap-1 h-9 px-4 py-2 has-[>svg]:px-3 bg-primary text-white rounded-md">
-              <StarsIcon className="size-4" />
-              <span className="hidden md:block">Tools</span>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-48">
-              <DropdownMenuItem asChild>
-                <Link href="/resume" className="flex items-center gap-2">
-                  <FileText className="h-4 w-4" />
-                  Resume Builder
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/cover-letter" className="flex items-center gap-2">
-                  <PenBox className="h-4 w-4" />
-                  Cover Letter
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/interview" className="flex items-center gap-2">
-                  <GraduationCap className="h-4 w-4" />
-                  Interview Prep
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <DropDown />
         </SignedIn>
         <SignedOut>
           <SignInButton>
