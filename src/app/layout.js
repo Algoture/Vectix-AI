19f797ed307cec1ff6e650f2d5fba3b4fde87885
Umesh Nagare <umeshnagare27@gmail.com>
2025-03-23 15:35:31 +0530

    add : clerk auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
+import Header from "@/components/custom/Header";
+import Footer from "@/components/custom/Footer";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Vectix AI",
@@ -13,12 +14,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.variable} antialiased`}
+        >
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import { Logo } from "./Icons";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="fixed top-4 w-full cen">
+      <nav className="bg-gray-200 rounded-lg w-fit p-2">
+        <Link href="/" className="cen">
+          <Logo className="size-7 fill-primary" />
+          <p>Vectix AI</p>
+        </Link>
+      </nav>
+    </header>
+  );
 }
