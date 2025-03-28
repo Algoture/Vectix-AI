9ba34643455fe62706d35f10fcd8f3dac606ee41
Umesh Nagare <umeshnagare27@gmail.com>
2025-03-28 15:55:04 +0530

    update : footer n header

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-muted/50 py-4">
-      <div className="container mx-auto px-4 text-center text-gray-200">
+      <div className="container mx-auto px-4 text-center text-muted-foreground">
         <p>Vectix AI</p>
       </div>
     </footer>
--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -9,20 +9,28 @@ export default function Header() {
   return (
     <div className="relative">
       <div className="fixed top-5 z-20 flex w-full justify-center">
-        <nav className="bg-background/40  w-fit shadow-sm dark:border-gray-700 border border-border gap-5 backdrop-blur-md p-2  flex items-center rounded-md">
-          <ul className="flex gap-4 items-center cursor-pointer">
+        <nav className="bg-background/50  w-fit  dark:border-gray-700 border border-border gap-5 backdrop-blur-xl p-2  flex items-center rounded-md">
+          <ul className="flex gap-20 items-center cursor-pointer">
             <li>
               <Link href="/" className="flex items-center pl-1">
-                {/* <Logo className="fill-primary size-6 rotate-180" /> */}
-                <p>Vectix AI</p>
+                <Logo className="fill-primary dark:fill-white size-6 rotate-180" />
+                <p className="text-black dark:text-white font-semibold">
+                  Vectix AI
+                </p>
               </Link>
             </li>
-            <Link href={"/pricing"} className="text-muted-foreground">
-              Pricing
-            </Link>
-            <Link href={"/support"} className="text-muted-foreground -mr-2">
-              Support
-            </Link>
+            <li className="flex gap-4">
+              <Link
+                href={"/pricing"}
+                className="text-neutral-800 dark:text-neutral-200">
+                Pricing
+              </Link>
+              <Link
+                href={"/support"}
+                className="text-neutral-800 dark:text-neutral-200 -mr-2">
+                Support
+              </Link>
+            </li>
           </ul>
 
           <div className="flex gap-2 items-center">
