3f7a58fb254fc7b97895248b8540a54e6888e96c
Umesh Nagare <umeshnagare27@gmail.com>
2025-04-08 07:20:18 +0530

    modify : server actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -11,7 +11,7 @@ export async function getAuthenticatedUser() {
         await connectDB();
         const mongoUser = await User.findOne({ clerkUserId: userId });
         if (!mongoUser) return { success: false, error: "User not found." };
-        return { success: true, user: mongoUser.toObject(), clerkUserId: userId };
+        return { success: true, user: mongoUser, clerkUserId: userId };
     } catch (err) {
         return { success: false, error: "Server error: " + err.message };
     }
