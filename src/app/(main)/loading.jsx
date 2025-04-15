dd5fd89d5fd44ae9f97365d734a707e09aeaeecc
Umesh Nagare <umeshnagare27@gmail.com>
2025-04-15 15:18:11 +0530

    add : loading state

---
+++ src/app/(main)/loading.jsx
@@ -0,0 +1,11 @@
+import Loader from "@/components/custom/Loader";
+
+const Loading = () => {
+  return (
+    <div className="w-full min-h-screen cen">
+      <Loader />
+    </div>
+  );
+};
+
+export default Loading;
