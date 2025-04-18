2c8e0744634c5c6ccc4fbe960fff6b75949d5d3a
Umesh Nagare <umeshnagare27@gmail.com>
2025-04-18 11:08:14 +0530

    add : generate and Display questions

--- src/app/(main)/voice-interview/interview/page.jsx
+++ src/app/(main)/voice-interview/interview/page.jsx
@@ -1,26 +1,55 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TakeInterview from "./TakeInterview";
-import { getVoiceQuestions } from "@/actions/interview";
+import {
+  genAndSaveInterviewQues,
+  getVoiceQuestions,
+} from "@/actions/interview";
+import { Button } from "@/components/ui/button";
+import { Brackets, Plus } from "lucide-react";
+import { toast } from "sonner";
 
-const page = () => {
+const VoiceInterviewPage = () => {
   const [data, setData] = useState([]);
-  useEffect(() => {
-    async function voice() {
+  async function voice() {
+    try {
       const fetchedata = await getVoiceQuestions();
+      toast.success("Questions Fetched Successfully!");
       setData(fetchedata);
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to Fetch Questions");
     }
-    voice();
-  }, []);
+  }
+  async function generateQues() {
+    try {
+      await genAndSaveInterviewQues();
+      toast.success("Questions Generated Successfully!");
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to Generate Questions");
+    }
+  }
 
   return (
     <>
       <TakeInterview />
-      {data.length>0 && data.map((ques,i) => {
-        return <p key={i}>{ques }</p>
-      })}
+      <div className="mt-4 cen gap-5">
+        <Button onClick={() => generateQues()}>
+          Generate <Plus />
+        </Button>
+        <Button onClick={() => voice()}>
+          Questions <Brackets />
+        </Button>
+      </div>
+      <div className="mt-5">
+        {data &&
+          data.map((ques, i) => {
+            return <p key={i}>{ques}</p>;
+          })}
+      </div>
     </>
   );
 };
 
-export default page;
+export default VoiceInterviewPage;
